f33f0b9328b42d14419c67f2ef5165376d80c360
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2025-09-15 22:02:19 +0700

    root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "@/components";
+import Navbar from "@/components/NavBar";
 import { UserProvider } from "./store/UserContext";
 import "./globals.css";
 
